f54978e397de1bc7b46ae0333e99fed104d01a68
v.zanfir <v.zanfir@corp.mail.ru>
2022-08-09 18:09:12 +0300

    Allow setting custom DefinitionProvider

--- src/EventsauceSerializerFactory.php
+++ src/EventsauceSerializerFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PhpSerialization\EventsauceSerializer;
 
+use EventSauce\ObjectHydrator\DefinitionProvider;
 use EventSauce\ObjectHydrator\ObjectMapperUsingReflection;
 use PhpSerialization\EventsauceSerializer\Cache\DefinitionCache;
 use PhpSerialization\EventsauceSerializer\Cache\ObjectCodeGeneratorDefinitionCache;
@@ -11,9 +12,9 @@ use PhpSerializer\Serializer\Serializer;
 
 final class EventsauceSerializerFactory
 {
-    public function createEventsauceSerializer(): Serializer
+    public function createEventsauceSerializer(?DefinitionProvider $definitionProvider = null): Serializer
     {
-        return new EventsauceSerializer(new ObjectMapperUsingReflection());
+        return new EventsauceSerializer(new ObjectMapperUsingReflection($definitionProvider));
     }
 
     /**
